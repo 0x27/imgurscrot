aa87c0ccb8182b9530ea00256c9cf46cfb0edc16
David Davidson <g00271860@gmit.ie>
2015-03-29 04:13:48 +0000

    Fix for https://github.com/0x27/imgurscrot/issues/2, raised by https://github.com/juanpabloaj :)

--- imgurscrot.py
+++ imgurscrot.py
@@ -3,7 +3,7 @@
 # takes a screenshot and uploads
 # to imgur using the imgur API
 # assuming you have an account
-# Version: 20150328.1
+# Version: 20150329.1
 # Author: Darren Martyn
 import pyscreenshot as pyscrot
 import requests
@@ -14,10 +14,7 @@ import time
 import os
 from StringIO import *
 
-# globals. alter these
-client_id = 'e32623091a466ab' # XXX: CHANGEME
-
-def upload_image(pil_img):
+def upload_image(pil_img, client_id):
     imgstr = StringIO()
     pil_img.save(imgstr, 'png')
     b64image = base64.standard_b64encode(imgstr.getvalue())
@@ -29,16 +26,21 @@ def upload_image(pil_img):
         print "[-] Image Upload Failed. Printing stack trace and exiting..."
         sys.exit(str(e))
     lol = json.loads(r.text)
-    print lol['data']['link'] #['id']['link']
+    print lol['data']['link']
 
 def main():
+    client_id_env_var = 'IMGUR_CLIENT_ID'
+    if client_id_env_var in os.environ.keys():
+        client_id = os.environ[client_id_env_var]
+    else:
+        sys.exit("[-] Set environmental variable IMGUR_CLIENT_ID to your client_id :)")
     try:
         image = pyscrot.grab()
     except Exception, e:
         print "[-] Error in screencapture. Printing stack trace and exiting..."
         sys.exit(str(e))
     try:
-        upload_image(pil_img=image)
+        upload_image(pil_img=image, client_id=client_id)
     except Exception, e:
         print "[-] Problem uploading screenshot... Printing stack trace and exiting..."
         sys.exit(str(e))
